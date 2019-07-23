de6e20d9bb5f765615d044ff6c5e22a4f7cc6e65
MMMikeM <50731334+MMMikeM@users.noreply.github.com>
2019-07-23 23:10:21 +0200

    Build files

---
+++ build/precache-manifest.714e8c8aed159782ce46609ec6b48a9b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f34487781116bb95e012ccf733af43b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a88b1f65cd3b973cff9",
+    "url": "/static/css/main.3d8ab9ba.chunk.css"
+  },
+  {
+    "revision": "dabef4b0fc69c8888bab",
+    "url": "/static/js/2.74005ed4.chunk.js"
+  },
+  {
+    "revision": "4a88b1f65cd3b973cff9",
+    "url": "/static/js/main.2d74ae9b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3b07bdbee083b6140c406bd2a9f7dd8b",
+    "url": "/static/media/BG.3b07bdbe.jpg"
+  },
+  {
+    "revision": "64fabf5502c57f1d2ea1f121b6a122a9",
+    "url": "/static/media/Splash.64fabf55.jpg"
+  }
+]);
